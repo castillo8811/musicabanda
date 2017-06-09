dae1714f46f78c2d8924e2e488e834a4c3b304db
Sergio Castillo <scastillo@arkusnexus.com>
2017-06-09 11:21:38 -0700

    Biografias y Videos

---
+++ themes/musica_banda/js/musicabanda.js
@@ -0,0 +1,15 @@
+/**
+ * Created by scastillo on 8/06/17.
+ */
+
+jQuery(document).ready(function(){
+
+    //Place Holder for
+    if(jQuery('.contact-message-feedback-form').length) {
+        jQuery('#edit-name').attr('placeholder', 'Tu nombre').width(250);
+        jQuery('#edit-mail').attr('placeholder', 'Tu correo electrónico').width(250);
+        jQuery('#edit-field-numero-de-telefonico-0-value').attr('placeholder', 'Número Telefónico').width(250);
+        jQuery('#edit-subject-0-value').attr('placeholder', 'Asunto');
+        jQuery('#edit-message-0-value').attr('placeholder', 'Cuéntanos, cómo te podemos ayudar?');
+    }
+});
