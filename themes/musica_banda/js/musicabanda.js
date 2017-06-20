41b0eb2750b08783dc976f8ad0811c45723ef0da
Sergio Castillo <scastillo@arkusnexus.com>
2017-06-20 00:27:55 -0700

    Ipad changes

--- themes/musica_banda/js/musicabanda.js
+++ themes/musica_banda/js/musicabanda.js
@@ -5,6 +5,27 @@
 jQuery(document).ready(function(){
 
     //Place Holder for
+
+    jQuery('.carrousel').slick({
+        centerMode: true,
+        centerPadding: '8   0px',
+        slidesToShow: 3,
+        dots: true,
+        focusOnSelect: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: false,
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
     if(jQuery('.contact-message-feedback-form').length) {
         jQuery('#edit-name').attr('placeholder', 'Tu nombre').width(250);
         jQuery('#edit-mail').attr('placeholder', 'Tu correo electrónico').width(250);
@@ -20,10 +41,10 @@ jQuery(document).ready(function(){
     if(jQuery(".path-node")){
         jQuery("#navbar").sticky({ topSpacing: 0 });
         jQuery('#navbar').on('sticky-start', function() {
-            jQuery('.brand-text').html(jQuery('.node-title').html()).addClass('node-head-text');
+            jQuery('header .brand-text').html(jQuery('.node-title').html()).addClass('node-head-text');
         });
         jQuery('#navbar').on('sticky-end', function() {
-            jQuery('.brand-text').html('Más Música Banda').removeClass('node-head-text');
+            jQuery(' header .brand-text').html('Más Música Banda').removeClass('node-head-text');
         });
     }
 
